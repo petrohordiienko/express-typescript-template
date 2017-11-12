78e99d9f95b79d9c6b2d78afcf61fa7e4095bdbe
Peter_V <gordienkopeter@gmail.com>
2017-11-12 22:08:49 +0200

    fix create user table migration (added default value)

--- src/database/migrations/create-user-table.migration.ts
+++ src/database/migrations/create-user-table.migration.ts
@@ -1,6 +1,6 @@
 const UsersMigration = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('Users', {
+        return queryInterface.createTable('users', {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -21,16 +21,18 @@ const UsersMigration = {
             },
             createdAt: {
                 allowNull: false,
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
             },
             updatedAt: {
                 allowNull: false,
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
             }
         });
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('Users');
+        return queryInterface.dropTable('users');
     }
 };
 
